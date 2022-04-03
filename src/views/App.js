b2c0fdabbf18b8e9c70f62fb89bb07bbf59bc285
devnhatlong <dev.nhatlong@gmail.com>
2022-04-04 03:07:07 +0700

    structure react basic

--- src/views/App.js
+++ src/views/App.js
@@ -1,12 +1,12 @@
 import logo from './logo.svg';
-import './App.css';
+import './App.scss';
 
 function App() {
   return (
     <div className="App">
       <header className="App-header">
         <img src={logo} className="App-logo" alt="logo" />
-        <p>Hello World</p>
+        <p>Hello World With React.Js (dev.NhatLong)</p>
         <a
           className="App-link"
           href="https://reactjs.org"
